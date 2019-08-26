e07e03bf8281d3f2b5a9cb179a3b90eed1ce85bf
请叫我猿叔叔 <1956025812@qq.com>
2019-08-26 18:02:14 +0800

    日志切面拦截 忽略Multipart类型的参数打印

--- src/main/java/com/authorization/privilege/aspect/LogAspect.java
+++ src/main/java/com/authorization/privilege/aspect/LogAspect.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,11 +27,12 @@ public class LogAspect {
     private final static Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
     // ..表示包及子包 该方法代表controller层的所有方法   注意： BaseController中的方法不需要执行切面 以及 import开头的方法不执行切面
-    @Pointcut("execution(public * com.authorization.privilege.controller..*.*(..)) " +
-            "&& !execution(public * com.authorization.privilege.controller.BaseController.*(..))" +
-            "&& !execution(public * com.authorization.privilege.controller..*.import*(..))")
+    @Pointcut("execution(public * com.authorization.privilege.controller..*.*(..)) "
+            + "&& !execution(public * com.authorization.privilege.controller.BaseController.*(..))"
+    )
     public void ControllerLog() {
     }
+
 
 
     @Before("ControllerLog()")
@@ -49,8 +51,12 @@ public class LogAspect {
         if (joinPoint.getArgs().length == 0) {
             requestLog.append("请求参数 = {} ");
         } else {
-            requestLog.append("请求参数 = " + new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                    .writeValueAsString(joinPoint.getArgs()[0]) + "");
+            if (joinPoint.getArgs()[0] instanceof MultipartFile || joinPoint.getArgs()[0] instanceof MultipartFile[]) {
+                requestLog.append("请求参数为文件上传，参数忽略");
+            } else {
+                requestLog.append("请求参数 = " + new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                        .writeValueAsString(joinPoint.getArgs()[0]) + "");
+            }
         }
 
         logger.info(requestLog.toString());
